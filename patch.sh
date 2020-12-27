0144cb086ab7a0200731f2da7b9c03a3724f86f1
Loïc Yhuel <loic.yhuel@gmail.com>
2020-12-27 21:16:28 +0100

    Add firmware patch script
    It allows to extract an image, change files, and repack.
    It runs all patches/*/patch.sh, which do the actual modifications.
    
    In addition to the extract.sh requirements, it needs either :
     - fakeroot (recommended)
     - to be run as root (not recommended)

---
+++ patch.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+
+IMAGE="$1"
+
+if [ -z "$IMAGE" ] || [ ! -e "$IMAGE" ]
+then
+    echo "Usage : $0 image"
+    exit 1
+fi
+
+if [ "$(id -u)" -ne 0 ]
+then
+    echo "Not running as root, using fakeroot"
+    exec fakeroot "$0" "$IMAGE"
+fi
+
+./extract.sh "$IMAGE"
+
+DIR="${IMAGE%.*}"
+ROOTFS="$DIR/squashfs-root"
+ROOTFS_ABS=$(realpath "$ROOTFS")
+
+printf "\nPatching rootfs ...\n"
+for PATCH_DIR in patches/*
+do
+    echo " - $PATCH_DIR :"
+    cd "$PATCH_DIR" && { ./patch.sh "$ROOTFS_ABS"; cd ../..; }
+done
+
+printf "\nCreating new squashfs image\n"
+mksquashfs "$ROOTFS" patched.squashfs -noappend -comp xz -b 262144
+
+OUT="${IMAGE%.*}_patched.bin"
+cat "$DIR/uImage" patched.squashfs > "$OUT"
+truncate -s 6M "$OUT"
+printf "\n%s created successfully\n" "$OUT"
+
+rm patched.squashfs
